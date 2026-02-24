f86ce3def3bc8201597a382a0ab953e6d2ab1452
Seo-JeongJin <tj0wjd9wls8@gmail.com>
2026-02-25 00:05:10 +0900

    data

--- server/data/tweet.js
+++ server/data/tweet.js
@@ -1,3 +1,4 @@
+// 메모리 내 임시 저장소 (실제 서비스에선 DB로 대체됨)
 let tweets = [
   {
     id: '1',
@@ -16,30 +17,35 @@ let tweets = [
   },
 ];
 
+// 데이터 전체 조회
 export async function getAll() {
   return tweets;
 }
 
+// 사용자 이름으로 필터링 조회
 export async function getAllByUsername(username) {
   return tweets.filter((tweet) => tweet.username === username);
 }
 
+// ID로 단일 조회
 export async function getById(id) {
   return tweets.find((tweet) => tweet.id === id);
 }
 
+// 데이터 생성 및 추가
 export async function create(text, name, username) {
   const tweet = {
-    id: Date.now().toString(),
+    id: Date.now().toString(), // 임시 ID 생성
     text,
     createdAt: new Date(),
     name,
     username,
   };
-  tweets = [tweet, ...tweets];
+  tweets = [tweet, ...tweets]; // 배열 맨 앞에 추가
   return tweet;
 }
 
+// 데이터 수정
 export async function update(id, text) {
   const tweet = tweets.find((tweet) => tweet.id === id);
   if (tweet) {
@@ -48,6 +54,7 @@ export async function update(id, text) {
   return tweet;
 }
 
+// 데이터 삭제
 export async function remove(id) {
   tweets = tweets.filter((tweet) => tweet.id !== id);
 }
